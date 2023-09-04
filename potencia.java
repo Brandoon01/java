85f7a8e320d25b3fddefbfea317fc405ecb50400
brandon ruiz <142250235+Brandoon01@users.noreply.github.com>
2023-09-04 16:57:48 -0500

    Add files via upload

---
+++ potencia.java
@@ -0,0 +1,18 @@
+public class potencia{
+
+    int exponente=3;
+    int potencia=4;
+    int respuesta=0;
+
+    public void calcular() {
+        respuesta=(exponente * (exponente-1) * exponente);
+        System.out.println("El resultado es: "+respuesta);
+        }
+
+    public static void main(String[] args) {
+        potencia r = new potencia();
+        r.calcular(); 
+        System.out.println("El resultado es: "+r);
+    }
+
+}
